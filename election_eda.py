23e3616731e5f1d5b08aae7355499a9c5fd1716f
RyanG73 <34326964+RyanG73@users.noreply.github.com>
2020-11-16 21:55:03 -0500

    imports

--- election_eda.py
+++ election_eda.py
@@ -2,4 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-print('hello')
+gov_county = pd.read_csv('/Users/ryangerda/PycharmProjects/elections/us-election-2020/governors_county.csv')
+
+
+house_candidate = pd.read_csv('us-election-2020/house_candidate.csv')
+house_state = pd.read_csv('us-election-2020/house_state.csv')
+
+pres_county_candidate = pd.read_csv('us-election-2020/president_county_candidate.csv')
+pres_county = pd.read_csv('us-election-2020/president_county.csv')
+
+
+
+ohio = pres_county_candidate[pres_county_candidate['state'] == 'Ohio']
